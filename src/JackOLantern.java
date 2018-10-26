37056eb26ef92d1e0408377eb5f95afebf5af2b4
BT_1N3_31@bktech.local <BT_1N3_31@bktech.local>
2018-10-26 10:15:31 -0400

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,35 @@
+public class JackOLantern extends Object{
+    String [][] faceFeatures;
+    JackOLantern(String [][] faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+    public void edit (String replace, int row, int column)
+    {
+        faceFeatures[row][column] = replace;
+    }
+
+    public void fill(String str)
+    {
+        for (int i = 0; i < faceFeatures.length; i++)
+        {
+            for (int j = 0; j < faceFeatures[i].length; j++)
+            {
+                faceFeatures[i][j]= str;
+            }
+        }
+    }
+
+    public String toString()
+    {
+        String JackOLantern = "";
+        for (int i = 0; i < faceFeatures.length; i++)
+        {
+            for (int j = 0; j < faceFeatures[i].length; j++)
+            {
+                JackOLantern += faceFeatures[i][j] + "\n";
+            }
+        }
+        return  JackOLantern;
+    }
+}
---
+++ src/JackOLanternRunner.java
@@ -0,0 +1,6 @@
+public class JackOLanternRunner{
+    public static void main(String[] args)
+    {
+        JackOLantern faces = new JackOLantern[][];
+    }
+}
